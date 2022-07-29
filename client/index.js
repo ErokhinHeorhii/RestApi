1ca58b4b65bd3443243693aa9bf194bee901d6a3
George Erokhin <Georgii070684@gmail.com>
2022-07-29 15:56:37 +0300

    feat: add function getEmployee

--- client/index.js
+++ client/index.js
@@ -20,12 +20,25 @@ const getEmployees = async (queryParams) => {
   return { items, count };
 };
 
-const main = async () => {
-  const employee = await getEmployees([
-    { key: "_page", value: 1 },
-    { key: "_limit", value: 2 },
-  ]);
+const getEmployee = async (id) => {
+  const res = await fetch(
+    `${baseUrl}${path.employee}/${id}`);
+  const employee = await res.json();
+  return employee;
+};
 
+
+// const main = async () => {
+//   const employee = await getEmployees([
+//     { key: "_page", value: 1 },
+//     { key: "_limit", value: 2 },
+//   ]);
+
+//   console.log(employee);
+// };
+
+const main = async () => {
+  const employee = await getEmployee(5);
   console.log(employee);
 };
 main();
