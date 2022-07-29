e8586765e1ab81bbfa5e81d6b06e56a495151595
George Erokhin <Georgii070684@gmail.com>
2022-07-29 12:42:13 +0300

    feat: add file for start proect2

--- client/index.js
+++ client/index.js
@@ -1,17 +1,22 @@
-const baseUrl="http://localhost:3000";
-const path ={
-   employee: '/employees',
-   comments: '/comments',
-}
-
-const generateQueryString =(queryParams=[]) => queryParams.length ? `?${queryParams.map(x=> `${x.key}=${x.value}`).join('&')}` : '';
-
-// [{key: 'role', value: "manager"}]
-const getEmployee = async(queryParams)=>{
-  const res =  await fetch(`${baseUrl}${path.employee}${generateQueryString(queryParams)}`);
-  const data = await res.json();
-  console.log(data);
-
+const baseUrl = "http://localhost:3000";
+const path = {
+  employee: "/employees",
+  comments: "/comments",
 };
 
-getEmployee();
+const generateQueryString = (queryParams = []) => {
+  return queryParams.length
+    ? `?${queryParams.map((x) => `${x.key}=${x.value}`).join("&")}`
+    : "";
+};
+
+// [{key: 'role', value: "manager"}]
+const getEmployee = async (queryParams) => {
+  const res = await fetch(
+    `${baseUrl}${path.employee}${generateQueryString(queryParams)}`
+  );
+  const data = await res.json();
+  console.log(data);
+};
+
+getEmployee([{key: 'role', value: "menager"}, {key: 'name', value: "Marc"}]);
